f677f3d7329f4529d120d1622aa278441f554b8a
RafaelGDS <rafly2012@hotmail.com>
2020-03-17 10:54:36 -0300

    reusando instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(1, "Rafael", "rafael.gds@yahoo.com", new Date(), (float) 1.300, obj);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println("=== TEST 1: seller finfById ===");
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 
 	}
